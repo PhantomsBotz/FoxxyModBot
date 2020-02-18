d09ea4977a76b348862199704edcd011eb8dcb67
PhantomsBotz <60788235+PhantomsBotz@users.noreply.github.com>
2020-02-17 22:19:20 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const FS = require("fs");
+//Consts
+
+var prefix = "f!"
+bot.commands = new Discord.Collection();
+//Vars
+
+fs.readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+bot.on("ready", () => {
+  console.log("ready")
+  bot.user.setActivity("DM me for help! -- Testing")
+})
+
+  bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (message.author.bot) return;
+  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(prefix)) return;
+  if (cmd) {
+    cmd.run(bot, message, args, Discord, con)
+    console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+    
+  }
+})
+
+bot.login(process.env.Token)
